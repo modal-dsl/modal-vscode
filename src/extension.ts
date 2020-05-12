14375f74904ec590f0864592da09fd5b20315fc1
Jonathan Neugebauer <jonathan.n@uni-muenster.de>
2020-05-12 23:19:18 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,55 @@
+'use strict';
+
+import * as path from 'path';
+import * as os from 'os';
+
+import {Trace} from 'vscode-jsonrpc';
+import { commands, window, workspace, ExtensionContext, Uri } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+    // The server is a locally installed in src/mdal
+    let launcher = os.platform() === 'win32' ? 'mdal-standalone.bat' : 'mdal-standalone';
+    let script = context.asAbsolutePath(path.join('src', 'mdal', 'bin', launcher));
+
+    let serverOptions: ServerOptions = {
+        run : { command: script },
+        debug: { command: script, args: [], options: { env: createDebugEnv() } }
+    };
+    
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['mdal'],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    };
+    
+    // Create the language client and start the client.
+    let lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
+    
+    var disposable2 =commands.registerCommand("mdal.a.proxy", async () => {
+        let activeEditor = window.activeTextEditor;
+        if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'mdal') {
+            return;
+        }
+
+        if (activeEditor.document.uri instanceof Uri) {
+            commands.executeCommand("mdal.a", activeEditor.document.uri.toString());
+        }
+    })
+    context.subscriptions.push(disposable2);
+    
+    // enable tracing (.Off, .Messages, Verbose)
+    lc.trace = Trace.Verbose;
+    let disposable = lc.start();
+    
+    // Push the disposable to the context's subscriptions so that the 
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
+}
+
+function createDebugEnv() {
+    return Object.assign({
+        JAVA_OPTS:"-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y"
+    }, process.env)
+}
